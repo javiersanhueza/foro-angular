00872fa6048622ef9b7e33737f6a44294dbbb59b
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-07-26 12:40:15 -0400

    Preparar el componente de registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/user';
+
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public pageTitle: string;
+  public user: User;
+  constructor() {
+    this.pageTitle = 'Registrate';
+    this.user = new User(
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      'ROLE_USER'
+    );
+  }
 
   ngOnInit(): void {
   }
