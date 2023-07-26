b01bca207cbc72a326edc1f860e2fa58010f042e
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-07-26 17:58:03 -0400

    Mantener al usuario en la sesión

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
@@ -20,7 +21,9 @@ export class LoginComponent implements OnInit {
   public isLoadingLogin: boolean;
   public identity: User;
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ) {
     this.status = '';
     this.token = '';
@@ -56,13 +59,17 @@ export class LoginComponent implements OnInit {
     this._userService.signUp(this.user).subscribe(
       response => {
         if (response.user && response.user._id) {
-          this.status = 'success';
           this.identity = response.user;
+          localStorage.setItem('identity', JSON.stringify(this.identity));
           // Conseguir token identificado
           this._userService.signUp(this.user, true).subscribe(
             response => {
               if (response.token) {
                 this.token = response.token;
+                localStorage.setItem('token', this.token);
+
+                this.status = 'success';
+                this._router.navigate(['index']);
               }
             },
             error => {
