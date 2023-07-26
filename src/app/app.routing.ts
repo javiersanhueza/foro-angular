493b914401ebf35f6142f5d53ae3360e2bc030f3
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-07-26 12:30:40 -0400

    Página de inicio

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,10 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 // Importar los componentes
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 // Array de rutas
 const appRoutes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'index', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: LoginComponent }
