51368c6271502b1e2ac903a970fb02df557fa835
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-07-26 15:15:58 -0400

    Crear método de registro

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,13 +3,27 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { User } from '../models/user';
+import { global } from './global';
 
 @Injectable()
 export class UserService {
+  public url: string;
   constructor(
     private _http: HttpClient
   ) {
+    this.url = global.url;
   }
 
+  register(user: User): Observable<any> {
+    // Convertir el objeto del usuario a un json string
+    const body = JSON.stringify(user);
+
+    // Definir las cabeceras
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+
+    // Hacer la petición ajax
+    return this._http.post(`${this.url}/register`, body, { headers })
+  }
 
 }
