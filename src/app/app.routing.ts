453912a58d7a7b3d2c37417c03546f9f0f3da057
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-07-27 10:31:49 -0400

    Crear la página de ajustes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
 
 // Array de rutas
 const appRoutes: Routes = [
@@ -13,6 +14,7 @@ const appRoutes: Routes = [
   { path: 'index', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'settings', component: UserEditComponent },
   { path: '**', component: LoginComponent }
 ];
 
