79cdef869d366b21a8d60aeb24086425d6d4c37a
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-08-14 12:55:16 -0400

    Componente de listado de temas

--- src/app/panel/components/list/list.component.ts
+++ src/app/panel/components/list/list.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Router, Params } from '@angular/router';
+
+import { Topic } from '../../../models/topic';
+import { UserService } from '../../../services/user.service';
+import { TopicService } from '../../../services/topic.service';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [UserService, TopicService]
 })
 export class ListComponent implements OnInit {
+  public pageTitle: string = 'Mis temas';
+  public topics: Topic[] = [];
+  public identity: any;
+  public token: string;
+  public status: string = '';
+  public isLoadingTopics: boolean = false;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService,
+    private _topicService: TopicService
+  ) {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 
   ngOnInit(): void {
   }
